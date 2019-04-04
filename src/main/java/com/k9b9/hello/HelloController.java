0310f8784afd92b3641ed388a6147f2e669987d9
KevinB <k1@k9b9.com>
2019-04-04 14:25:54 -0400

    Now using port 80 as default port, print additional information to distinguish reloads and host

--- src/main/java/com/k9b9/hello/HelloController.java
+++ src/main/java/com/k9b9/hello/HelloController.java
@@ -1,5 +1,8 @@
 package com.k9b9.hello;
 
+import java.net.InetAddress;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,16 @@ public class HelloController {
 
     @GetMapping("/")
     public String home(Model model) {
+        String hostname = System.getenv("HOSTNAME");
+        if (hostname==null) {
+            try {
+                hostname = InetAddress.getLocalHost().getHostName();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        model.addAttribute("hostname", hostname==null ? "" : hostname);
+        model.addAttribute("timestamp", (new Date()).toString());
         return "hello";
     }
 }
